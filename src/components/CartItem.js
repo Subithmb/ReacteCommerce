6d1d9b86a5e97b1a7aaf534d59384328f254e0d0
subith <bijusubith@gmail.com>
2023-07-19 23:23:03 +0530

    second commit of react eCommerce application

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import {IoMdAdd, IoMdClose, IoMdRemove}from 'react-icons/io'
+import {CartContext}from'../contexts/CartContext'
 
 const CartItem = ({item}) => {
+  const{removeFromCart,increaseAmount,decreaseAmount}=useContext(CartContext)
   const{id,title,image,price,amount}=item
  
   return <div className='flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500'>
@@ -18,7 +20,7 @@ const CartItem = ({item}) => {
         <Link className='text-sm uppercase font-medium max-w-[240px]
          text-primary hover:underline' to={`/product/${id}`}>{title}</Link>
          {/* {icon for removing} */}
-         <div className='text-xl cursor-pointer'>
+         <div onClick={()=>removeFromCart(id)} className='text-xl cursor-pointer'>
           <IoMdClose className='text-gray-500 hover:text-red-500 transition'/>
         </div>
          </div>
@@ -26,13 +28,13 @@ const CartItem = ({item}) => {
           {/* {qty} */}
           <div className=' flex flex-1 max-w-[100px] items-center h-full border text-primary font-medium'>
             {/* {minus} */}
-            <div className='flex-1 h-full flex justify-center items-center cursor-pointer'>
+            <div onClick={()=>decreaseAmount(id)} className='flex-1 h-full flex justify-center items-center cursor-pointer'>
             <IoMdRemove />
             </div>
             {/* {amount} */}
             <div className='h-full flex justify-center items-center px-2'>{amount}</div>
             {/* {adding} */}
-            <div className='flex-1 h-full flex justify-center items-center cursor-pointer'>
+            <div onClick={()=>increaseAmount(id)} className='flex-1 h-full flex justify-center items-center cursor-pointer'>
             <IoMdAdd />
             </div>
 
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,8 +1,27 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 export const CartContext=createContext()
 const CartProvider = ({children}) => {
 
   const[cart,setCart]=useState([])
+  const[itemAmount,setItemAmount]=useState(0)
+  const[total,setTotal]=useState(0)
+
+  useEffect(()=>{
+    if(cart){
+      const totalAmount=cart.reduce((acc,cur)=>{
+        return acc+cur.price*cur.amount
+      },0)
+      setTotal(totalAmount)
+    }
+  },[cart])
+  useEffect(()=>{
+    if(cart){
+      const amount=cart.reduce((acc,cur)=>{
+        return acc+cur.amount
+      },0)
+      setItemAmount(amount)
+    }
+  },[cart])
 
   const addToCart=(product,id)=>{
 
@@ -30,7 +49,49 @@ const CartProvider = ({children}) => {
     }
     console.log(cart,'added to the cart');
   }
-  return <CartContext.Provider value={{addToCart,cart}}>
+
+  //removing cart
+
+const removeFromCart =(id)=>{
+  const newCart=cart.filter((item)=>{
+    return item.id !== id
+  })
+  setCart(newCart)
+}
+
+// clear cart
+const clearCart =()=>{
+  setCart([])
+}
+
+
+// increase amount
+const increaseAmount =(id)=>{
+  const cartItem=cart.find((item)=>item.id===id)
+ addToCart(cartItem,id)
+}
+
+// decrease amount
+const decreaseAmount =(id)=>{
+  const cartItem=cart.find((item)=>{return item.id===id})
+ if(cartItem){
+  const newCart=cart.map((item)=>{
+    if(item.id===id){
+      return{...item,amount:cartItem.amount-1}
+    }else{
+      return item
+    }
+  })
+  setCart(newCart)
+ }
+  if(cartItem.amount<2){
+    removeFromCart(id)
+  }
+ 
+}
+
+   
+  return <CartContext.Provider value={{addToCart,cart,removeFromCart,clearCart,increaseAmount,decreaseAmount,itemAmount,total}}>
     {children}
   </CartContext.Provider>
 };
